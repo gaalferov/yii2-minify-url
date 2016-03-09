3e88719ae76e42e1e560ef995071057fb4b912af
gaalferov <admin@gaalferov.com>
2016-03-09 22:15:39 +0200

    add curl options

--- models/NixShortUrls.php
+++ models/NixShortUrls.php
@@ -108,6 +108,16 @@ class NixShortUrls extends \yii\db\ActiveRecord
     public function checkUrl($url)
     {
         $curl = new curl\Curl();
+
+        $curl->setOption(CURLOPT_SSL_VERIFYHOST, false);
+        $curl->setOption(CURLOPT_FOLLOWLOCATION, true);
+        $curl->setOption(CURLOPT_RETURNTRANSFER, true);
+        $curl->setOption(CURLOPT_AUTOREFERER, true);
+        $curl->setOption(CURLOPT_CONNECTTIMEOUT, 10);
+        $curl->setOption(CURLOPT_TIMEOUT, 10);
+        $curl->setOption(CURLOPT_MAXREDIRS, 10);
+        $curl->setOption(CURLOPT_USERAGENT, 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.89 Safari/537.36');
+
         $curl->get($url);
 
         if ($curl->responseCode != 200)
