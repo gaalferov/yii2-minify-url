27cdac96bf02d7b325f6c70bebdae69024ac34d5
Alforov Hennadii <hennadii.a@gvocom.com>
2017-03-09 17:47:09 +0200

    init new tests settings

---
+++ tests/_support/Helper/ConfigHelper.php
@@ -0,0 +1,14 @@
+<?php
+namespace Helper;
+
+class ConfigHelper extends \Codeception\Module
+{
+  public function getConfig($key)
+  {
+    if (isset($this->config[$key])) {
+      return $this->config[$key];
+    } else {
+      return null;
+    }
+  }
+}
--- tests/acceptance/IndexPageCest.php
+++ tests/acceptance/IndexPageCest.php
@@ -1,31 +1,53 @@
 <?php
+
+/**
+ * Class IndexPageCest
+ */
 class IndexPageCest
 {
-    public function _before(AcceptanceTester $I)
-    {
-        $I->amOnPage('/');
+  /**
+   * @param AcceptanceTester $I
+   * @before
+   */
+  public function _before(AcceptanceTester $I, \Codeception\Scenario $scenario)
+  {
+    $I->amOnPage('/');
+    if ($scenario->current('browser') === 'phantomjs') {
+      $I->maximizeWindow();
     }
+  }
 
-    public function _after(AcceptanceTester $I)
-    {
-    }
+  protected function SetLang(AcceptanceTester $I)
+  {
+    $I->sendAjaxPostRequest('/lang', array('language' => 'en'));
+  }
 
-    // tests
-    public function testUrlField(AcceptanceTester $I)
-    {
-        $I->see('Paste your original URL here');
+  /**
+   * @param AcceptanceTester $I
+   * @before  setAffiliateCookie
+   */
+  public function testUrlField(AcceptanceTester $I)
+  {
+    $I->see('Paste your original URL here');
+  }
 
-    }
-    public function testIncorrectUrl(AcceptanceTester $I)
-    {
-        $I->fillField('NixShortUrls[long_url]', 'http://notcorrecturlrrrrrrr.asd');
-        $I->click('Shorten URL');
-        $I->see('Something is wrong with your URL');
-    }
-    public function testCorrectUrl(AcceptanceTester $I)
-    {
-        $I->fillField('NixShortUrls[long_url]', 'http://codeception.com');
-        $I->click('Shorten URL');
-        $I->see('http://codeception.com');
-    }
+  /**
+   * @param AcceptanceTester $I
+   */
+  public function testIncorrectUrl(AcceptanceTester $I)
+  {
+    $I->fillField('NixShortUrls[long_url]', 'http://notcorrecturlrrrrrrr.asd');
+    $I->click('Shorten URL');
+    $I->see('Something is wrong with your URL');
+  }
+
+  /**
+   * @param AcceptanceTester $I
+   */
+  public function testCorrectUrl(AcceptanceTester $I)
+  {
+    $I->fillField('NixShortUrls[long_url]', 'http://codeception.com');
+    $I->click('Shorten URL');
+    $I->see('http://codeception.com');
+  }
 }
