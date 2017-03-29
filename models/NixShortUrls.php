1dad3f55c85ffb51a181d9198ef0c1bff793aac0
gaalferov <admin@gaalferov.com>
2017-03-29 10:27:01 +0300

    hotfix

--- models/NixShortUrls.php
+++ models/NixShortUrls.php
@@ -167,8 +167,9 @@ class NixShortUrls extends \yii\db\ActiveRecord
     $curl->setOption(CURLOPT_TIMEOUT, 5);
     $curl->setOption(CURLOPT_MAXREDIRS, 10);
     $curl->setOption(CURLOPT_USERAGENT, 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.89 Safari/537.36');
+    $curl->get($url);
 
-    return $curl->get($url);
+    return $curl;
   }
 
   /**
