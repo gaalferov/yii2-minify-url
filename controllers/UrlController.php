a57ac2315f4ddb29391580b3f8ffcdc51a6e9f3a
gaalferov <admin@gaalferov.com>
2017-07-02 14:10:41 +0300

    PSR

--- controllers/UrlController.php
+++ controllers/UrlController.php
@@ -46,7 +46,7 @@ class UrlController extends Controller
             'allow' => true,
             'actions' => ['add'],
             'verbs' => ['POST'],
-            'matchCallback' => function ($rule, $action) {
+            'matchCallback' => function () {
               return !Yii::$app->user->isGuest ? Yii::$app->user->can('urlCreate') : true;
             }
           ],
@@ -69,6 +69,7 @@ class UrlController extends Controller
   /**
    * Lists all NixShortUrls models.
    * @return mixed
+   * @throws \yii\base\InvalidParamException
    */
   public function actionIndex()
   {
@@ -87,6 +88,7 @@ class UrlController extends Controller
    * If update is successful, the browser will be redirected to the 'view' page.
    * @param integer $id
    * @return mixed
+   * @throws \yii\base\InvalidParamException
    */
   public function actionUpdate($id)
   {
@@ -94,11 +96,11 @@ class UrlController extends Controller
 
     if ($model->load(Yii::$app->request->post()) && $model->save()) {
       return $this->redirect(['index']);
-    } else {
-      return $this->render('update', [
-        'model' => $model,
-      ]);
     }
+
+    return $this->render('update', [
+        'model' => $model,
+    ]);
   }
 
   /**
@@ -106,6 +108,8 @@ class UrlController extends Controller
    * If deletion is successful, the browser will be redirected to the 'index' page.
    * @param integer $id
    * @return mixed
+   * @throws NotFoundHttpException
+   * @throws \yii\db\StaleObjectException
    */
   public function actionDelete($id)
   {
@@ -123,15 +127,16 @@ class UrlController extends Controller
    */
   protected function findModel($id)
   {
-    if (($model = NixShortUrls::findOne($id)) !== null) {
-      return $model;
-    } else {
-      throw new NotFoundHttpException('The requested page does not exist.');
+    if (($model = NixShortUrls::findOne($id)) === null) {
+        throw new NotFoundHttpException('The requested page does not exist.');
     }
+
+    return $model;
   }
 
   /**
    * @throws \yii\web\HttpException
+   * @throws \yii\base\InvalidParamException
    */
   public function actionAdd()
   {
@@ -162,7 +167,7 @@ class UrlController extends Controller
    * @return \yii\web\Response
    * @throws \yii\web\HttpException
    * @throws \yii\web\NotAcceptableHttpException
-   * @throws \yii\web\NotFoundHttpException
+   * @throws NotFoundHttpException
    */
   public function actionForward($code)
   {
