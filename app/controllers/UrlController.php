94d283720b408f79522548a828affcbd2f1fc87b
Hennadii Alforov <admin@gaalferov.com>
2019-12-19 17:31:54 +0200

    description-field > optimization UrlController

--- app/controllers/UrlController.php
+++ app/controllers/UrlController.php
@@ -9,188 +9,200 @@ use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\AccessControl;
 use app\models\NixUserInfo;
+use yii\web\Response;
+use yii\captcha\CaptchaAction;
+use yii\web\ErrorAction;
 
 /**
  * UrlController implements the CRUD actions for NixShortUrls model.
  */
 class UrlController extends Controller
 {
-
-  /**
-   * @return array
-   */
-  public function actions()
-  {
-    return [
-      'error' => [
-        'class' => 'yii\web\ErrorAction',
-      ],
-      'captcha' => [
-        'class' => 'yii\captcha\CaptchaAction',
-        'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-      ],
-    ];
-  }
-
-  /**
-   * @return array
-   */
-  public function behaviors()
-  {
-    return [
-      'access' => [
-        'class' => AccessControl::className(),
-        'only' => ['index', 'view', 'update', 'delete', 'add'],
-        'rules' => [
-          [
-            'allow' => true,
-            'actions' => ['add'],
-            'verbs' => ['POST'],
-            'matchCallback' => function () {
-              return !Yii::$app->user->isGuest ? Yii::$app->user->can('urlCreate') : true;
-            }
-          ],
-          [
-            'allow' => true,
-            'actions' => ['delete'],
-            'verbs' => ['POST'],
-            'roles' => ['urlManage'],
-          ],
-          [
-            'allow' => true,
-            'actions' => ['index', 'update'],
-            'roles' => ['urlManage'],
-          ],
-        ],
-      ],
-    ];
-  }
-
-  /**
-   * Lists all NixShortUrls models.
-   * @return mixed
-   * @throws \yii\base\InvalidParamException
-   */
-  public function actionIndex()
-  {
-      $searchModel = new NixShortUrlsSearch();
-      $dataProvider = $searchModel->search(Yii::$app->request->getQueryParams());
-
-      return $this->render('index', [
-          'dataProvider' => $dataProvider,
-          'searchModel' => $searchModel,
-      ]);
-  }
-
-  /**
-   * Updates an existing NixShortUrls model.
-   * If update is successful, the browser will be redirected to the 'view' page.
-   * @param integer $id
-   * @return mixed
-   * @throws \yii\base\InvalidParamException
-   */
-  public function actionUpdate($id)
-  {
-    $model = $this->findModel($id);
-
-    if ($model->load(Yii::$app->request->post()) && $model->save()) {
-      return $this->redirect(['index']);
+    /**
+     * @return array
+     */
+    public function actions(): array
+    {
+        return [
+            'error' => [
+                'class' => ErrorAction::class,
+            ],
+            'captcha' => [
+                'class' => CaptchaAction::class,
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
     }
 
-    return $this->render('update', [
-        'model' => $model,
-    ]);
-  }
-
-  /**
-   * Deletes an existing NixShortUrls model.
-   * If deletion is successful, the browser will be redirected to the 'index' page.
-   * @param integer $id
-   * @return mixed
-   * @throws NotFoundHttpException
-   * @throws \yii\db\StaleObjectException
-   */
-  public function actionDelete($id)
-  {
-    $this->findModel($id)->delete();
-
-    return $this->redirect(['index']);
-  }
-
-  /**
-   * Finds the NixShortUrls model based on its primary key value.
-   * If the model is not found, a 404 HTTP exception will be thrown.
-   * @param integer $id
-   * @return NixShortUrls the loaded model
-   * @throws NotFoundHttpException if the model cannot be found
-   */
-  protected function findModel($id)
-  {
-    if (($model = NixShortUrls::findOne($id)) === null) {
-        throw new NotFoundHttpException('The requested page does not exist.');
+    /**
+     * @return array
+     */
+    public function behaviors(): array
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['index', 'view', 'update', 'delete', 'add'],
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'actions' => ['add'],
+                        'verbs' => ['POST'],
+                        'matchCallback' => function () {
+                            return !Yii::$app->user->isGuest ? Yii::$app->user->can('urlCreate') : true;
+                        }
+                    ],
+                    [
+                        'allow' => true,
+                        'actions' => ['delete'],
+                        'verbs' => ['POST'],
+                        'roles' => ['urlManage'],
+                    ],
+                    [
+                        'allow' => true,
+                        'actions' => ['index', 'update'],
+                        'roles' => ['urlManage'],
+                    ],
+                ],
+            ],
+        ];
     }
 
-    return $model;
-  }
+    /**
+     * Lists all NixShortUrls models.
+     * @return string
+     */
+    public function actionIndex(): string
+    {
+        $searchModel = new NixShortUrlsSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->getQueryParams());
 
-  /**
-   * @throws \yii\web\HttpException
-   * @throws \yii\base\InvalidParamException
-   */
-  public function actionAdd()
-  {
-    $model_url = new NixShortUrls();
+        return $this->render(
+            'index',
+            [
+                'dataProvider' => $dataProvider,
+                'searchModel' => $searchModel,
+            ]
+        );
+    }
 
-    if (Yii::$app->request->post()) {
-      if ($model_url->load(Yii::$app->request->post()) && $model_url->validate()) {
-        if (!Yii::$app->user->isGuest) {
-          $model_url->setAttributes([
-            'user_id' => Yii::$app->user->id
-          ]);
+    /**
+     * Updates an existing NixShortUrls model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     *
+     * @param integer $id
+     *
+     * @return string
+     */
+    public function actionUpdate($id): string
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['index']);
         }
-        $model_url->checkUrl($model_url['long_url']);
-        $model_url->setAttributes([
-          'short_code' => $model_url->genShortCode(),
-          'time_create' => date("Y-m-d H:i:s")
-        ]);
-        $model_url->save();
-        Yii::$app->session->setFlash('success', Yii::t('burl', 'CREATED_NEW_SHORT_URL'));
-      }
+
+        return $this->render(
+            'update',
+            [
+                'model' => $model,
+            ]
+        );
     }
 
-    $this->redirect(Yii::$app->request->referrer);
-  }
+    /**
+     * Deletes an existing NixShortUrls model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     *
+     * @param integer $id
+     *
+     * @return Response
+     */
+    public function actionDelete($id): Response
+    {
+        $this->findModel($id)->delete();
 
-  /**
-   * @param $code
-   * @return \yii\web\Response
-   * @throws \yii\web\HttpException
-   * @throws \yii\web\NotAcceptableHttpException
-   * @throws NotFoundHttpException
-   */
-  public function actionForward($code)
-  {
-    $model_info = new NixUserInfo();
+        return $this->redirect(['index']);
+    }
 
-    $url = NixShortUrls::validateShortCode($code);
-    $url->updateCounters(['counter' => 1]);
+    /**
+     * Finds the NixShortUrls model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     *
+     * @param integer $id
+     *
+     * @return NixShortUrls the loaded model
+     * @throws NotFoundHttpException
+     */
+    protected function findModel($id): NixShortUrls
+    {
+        if (($model = NixShortUrls::findOne($id)) === null) {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
 
-    $user_info = parse_user_agent();
-    $user_ip = Yii::$app->geoip->ip();
+        return $model;
+    }
 
-    $model_info->setAttributes([
-      'short_url_id' => $url['id'],
-      'user_platform' => $user_info['platform'],
-      'user_agent' => $user_info['browser'],
-      'user_refer' => Yii::$app->request->referrer,
-      'user_ip' => Yii::$app->request->userIP,
-      'user_country' => $user_ip->country,
-      'user_city' => $user_ip->city,
-      'date' => date("Y-m-d")
-    ]);
-    $model_info->save();
+    /**
+     * Add Action
+     */
+    public function actionAdd(): Response
+    {
+        $model_url = new NixShortUrls();
 
-    return $this->redirect($url['long_url']);
-  }
+        if (Yii::$app->request->post()) {
+            if ($model_url->load(Yii::$app->request->post()) && $model_url->validate()) {
+                if (!Yii::$app->user->isGuest) {
+                    $model_url->setAttributes(
+                        [
+                            'user_id' => Yii::$app->user->id
+                        ]
+                    );
+                }
+                $model_url->checkUrl($model_url['long_url']);
+                $model_url->setAttributes(
+                    [
+                        'short_code' => $model_url->genShortCode(),
+                        'time_create' => date("Y-m-d H:i:s")
+                    ]
+                );
+                $model_url->save();
+                Yii::$app->session->setFlash('success', Yii::t('burl', 'CREATED_NEW_SHORT_URL'));
+            }
+        }
 
+        return $this->redirect(Yii::$app->request->referrer);
+    }
+
+    /**
+     * @param $code
+     *
+     * @return Response
+     */
+    public function actionForward($code): Response
+    {
+        $model_info = new NixUserInfo();
+
+        $url = NixShortUrls::validateShortCode($code);
+        $url->updateCounters(['counter' => 1]);
+
+        $user_info = parse_user_agent();
+        $user_ip = Yii::$app->geoip->ip();
+
+        $model_info->setAttributes(
+            [
+                'short_url_id' => $url['id'],
+                'user_platform' => $user_info['platform'],
+                'user_agent' => $user_info['browser'],
+                'user_refer' => Yii::$app->request->referrer,
+                'user_ip' => Yii::$app->request->userIP,
+                'user_country' => $user_ip->country,
+                'user_city' => $user_ip->city,
+                'date' => date('Y-m-d')
+            ]
+        );
+        $model_info->save();
+
+        return $this->redirect($url['long_url']);
+    }
 }
